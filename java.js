fd05b367354fbb39cd941e20f63a1e6c1b17c6eb
Rahul Kushwah <kusrahul069@gmail.com>
2025-04-05 21:48:36 +0530

    Initial commit

---
+++ java.js
@@ -0,0 +1,14 @@
+const classInfo={
+    rahul:{
+        enroll:"0901io201051",
+        status:"not Employed",
+        skillls:["c","c++","full-stack webdeveloper","DBMS","data engineer"],
+        passing:2024
+    },
+    sorav:{
+        enroll:"0901io201051",
+        status:"not Employed",
+        skillls:["c","c++","full-stack webdeveloper","DBMS","data engineer"],
+        passing:2024
+    },
+};
